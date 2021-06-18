39a1c16e395add52a18241d5f877d2dd74fa5e89
Basith P <basithpofficial@gmail.com>
2021-06-18 23:15:42 +0530

    Initial commit

---
+++ lib/views/todo_view.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:hivetodoapp/adapters/todo_adapter.dart';
+import 'package:hivetodoapp/views/add_todo.dart';
+
+class TodoView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(builder: (context) => AddTodo()),
+          );
+        },
+      ),
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        centerTitle: true,
+        title: Text(
+          "ToDo",
+          style: TextStyle(
+            color: Colors.yellow,
+            fontWeight: FontWeight.bold,
+            fontSize: 26,
+          ),
+        ),
+      ),
+      body: ValueListenableBuilder(
+        valueListenable: Hive.box<Todo>('todos').listenable(),
+        builder: (context, Box<Todo> box, _) {
+          if (box.values.isEmpty) {
+            return Center(
+              child: Text("No data available!",
+                  style: TextStyle(fontFamily: 'Montserrat')),
+            );
+          }
+          return ListView.builder(
+              itemCount: box.length,
+              itemBuilder: (context, index) {
+                Todo todo = box.getAt(index);
+                return ListTile(
+                  onLongPress: () async {
+                    await box.deleteAt(index);
+                  },
+                  title: Text(
+                    todo.title,
+                    style: TextStyle(fontSize: 20, fontFamily: 'Montserrat'),
+                  ),
+                  subtitle: Text(
+                    todo.description,
+                    style: TextStyle(fontSize: 16, fontFamily: 'Montserrat'),
+                  ),
+                );
+              });
+        },
+      ),
+    );
+  }
+}
